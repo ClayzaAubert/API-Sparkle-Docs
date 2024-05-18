a8863323c641ea5f093ca4c88c5ca231769fce45
Clayza <92805294+ClayzaAubert@users.noreply.github.com>
2024-05-19 06:04:36 +0700

    Update sparkle-saweria.js

--- sparkle-saweria.js
+++ sparkle-saweria.js
@@ -1,54 +1,60 @@
 const axios = require('axios');
+/* LIST SERVER DOMAIN
+ * https://rosalyn.my.id
+ * https://aubert.my.id
+ * https://clayza.my.id
+ */
+const SERVER_DOMAIN = 'https://clayza.biz.id';
 
 async function login(email, password) {
    try {
-      const response = await axios.post('https://SERVER_DOMAIN/api/saweria/login', { email, password });
+      const response = await axios.post(`${SERVER_DOMAIN}/api/saweria/login`, { email, password });
       return response.data;
    } catch (error) {
-      return error.response.data;
+      return error.response ? error.response.data : { error: 'Unknown error occurred' };
    }
 }
 
-async function createPayment(user_id, amount, msg) {
+async function createPayment(user_id, amount, name, email, msg) {
    try {
-      const response = await axios.post('https://SERVER_DOMAIN/api/saweria/createPayment', { user_id, amount, msg });
+      const response = await axios.post(`${SERVER_DOMAIN}/api/saweria/createPayment`, { user_id, amount, name, email, msg });
       return response.data;
    } catch (error) {
-      return error.response.data;
+      return error.response ? error.response.data : { error: 'Unknown error occurred' };
    }
 }
 
 async function checkPayment(user_id, payment_id) {
    try {
-      const response = await axios.get(`https://SERVER_DOMAIN/api/saweria/checkPayment/${user_id}/${payment_id}`);
+      const response = await axios.get(`${SERVER_DOMAIN}/api/saweria/checkPayment/${user_id}/${payment_id}`);
       return response.data;
    } catch (error) {
-      return error.response.data;
+      return error.response ? error.response.data : { error: 'Unknown error occurred' };
    }
 }
 
 async function getBalance(email, password) {
    try {
-      const response = await axios.post('https://SERVER_DOMAIN/api/saweria/balance', { email, password });
+      const response = await axios.post(`${SERVER_DOMAIN}/api/saweria/balance`, { email, password });
       return response.data;
    } catch (error) {
-      return error.response.data;
+      return error.response ? error.response.data : { error: 'Unknown error occurred' };
    }
 }
 
+// module.exports = { login, createPayment, checkPayment, getBalance };
+
 // Usage examples:
-
-// Login
-
 const email = '';
 const password = '';
 
+// Login
 login(email, password)
    .then(response => console.log('Login Response:', response))
    .catch(error => console.error('Login Error:', error));
 
-// Create Payment, Minimum Rp 10.000
-//createPayment('USER ID', 10000, 'MESSAGE')
+// Create Payment, Minimum Rp 1.000
+//createPayment('USER ID', 1000, 'NAME_DONATUR', 'EMAIL_DONATUR', 'MESSAGE')
 //   .then(response => console.log('Create Payment Response:', response))
 //   .catch(error => console.error('Create Payment Error:', error));
 
